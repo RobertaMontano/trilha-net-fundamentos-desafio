c147f196bc29e318645b9dcbdf058dbd149e0aab
Roberta <robertamontano_2013@hotmail.com>
2024-03-30 20:16:17 -0300

    Desafio Concluido

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -24,10 +24,10 @@ while (exibirMenu)
 {
     Console.Clear();
     Console.WriteLine("Digite a sua opção:");
-    Console.WriteLine("1 - Cadastrar veículo");
-    Console.WriteLine("2 - Remover veículo");
-    Console.WriteLine("3 - Listar veículos");
-    Console.WriteLine("4 - Encerrar");
+    Console.WriteLine("[1] - Cadastrar veículo");
+    Console.WriteLine("[2] - Remover veículo");
+    Console.WriteLine("[3] - Listar veículos");
+    Console.WriteLine("[4] - Encerrar");
 
     switch (Console.ReadLine())
     {
